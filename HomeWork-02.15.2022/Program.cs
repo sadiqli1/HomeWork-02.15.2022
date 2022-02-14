a507778711028e690dcaa289f31098fd1c0686a4
sadiqli1 <isaas@code.edu.az>
2022-02-15 00:57:08 +0400

    created folder

---
+++ HomeWork-02.15.2022/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeWork_02._15._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int count = 0;
+            int average = 0;
+            int[] arr = { 70, 65, 100, 90, 30, 95, 40 };
+            foreach (int num in arr)
+            {
+                sum += num;
+                count++;
+            }
+            average = sum / count;
+            Console.WriteLine(average);
+
+        }
+    }
+}
